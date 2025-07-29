7587c419876070d9d52b1fef185d2ebd96552de0
NomfundoFlorence <nomfundomlangeni0@gmail.com>
2025-07-29 23:50:02 +0200

    final commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,6 +30,16 @@ public_users.get("/", function (req, res) {
   return res.status(300).json({ message: "Yet to be implemented" });
 });
 
+// task10 - axios async-await
+public_users.get("/axios-async/books", async function (req, res) {
+  try {
+    const response = await axios.get("http://localhost:5000/");
+    return res.status(200).send(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching books", error });
+  }
+});
+
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   //Write your code here
@@ -37,6 +47,21 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
   res.send(JSON.stringify(books[isbn], null, 4));
   return res.status(300).json({ message: "Yet to be implemented" });
+});
+
+// task11 - axios async-await
+public_users.get("/axios-async/isbn/:isbn", async (req, res) => {
+  const isbn = req.params.isbn;
+
+  try {
+    const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error fetching book by ISBN",
+      error: error.message,
+    });
+  }
 });
 
 // Get book details based on author
@@ -56,6 +81,23 @@ public_users.get("/author/:author", function (req, res) {
   return res.status(300).json({ message: "Yet to be implemented" });
 });
 
+// task12 - axios async-await
+public_users.get("/axios-async/author/:author", async (req, res) => {
+  const author = req.params.author;
+
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/author/${encodeURIComponent(author)}`
+    );
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error fetching books by author",
+      error: error.message,
+    });
+  }
+});
+
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   //Write your code here
@@ -71,6 +113,21 @@ public_users.get("/title/:title", function (req, res) {
   }
 
   return res.status(300).json({ message: "Yet to be implemented" });
+});
+
+// task13 - axios async-await
+public_users.get("/axios-async/title/:title", async (req, res) => {
+  const title = req.params.title;
+
+  try {
+    const response = await axios.get(`http://localhost:5000/title/${encodeURIComponent(title)}`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error fetching books by title",
+      error: error.message,
+    });
+  }
 });
 
 //  Get book review
